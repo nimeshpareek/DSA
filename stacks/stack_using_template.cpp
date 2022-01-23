6e1df2dba73b77fca216e549993c543b35b8cf5a
nimeshpareek <72460673+nimeshpareek@users.noreply.github.com>
2022-01-23 22:54:15 +0530

    Add files via upload

---
+++ stacks/stack_using_template.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+#include<climits>
+using namespace std;
+
+template <typename T>
+struct stackusingarray{
+    T *arr;
+    int capacity;
+    int nextindex;
+
+    stackusingarray(int totalsize){
+    arr = new int[totalsize];
+    int capacity = totalsize;
+    nextindex = 0;
+    }
+
+    void push(T x){
+        if (nextindex == capacity)
+        {
+            cout << "Your stack is overflow ((-_-)) " << endl;
+            return;
+        }
+            
+            arr[nextindex] = x;
+            nextindex++;
+    }
+
+    int pop(){
+        if (nextindex == 0)
+        {
+            cout << "Your stack is Empty ((-_-))" << endl;
+            return 0;
+        }
+        else
+        {
+            nextindex--;
+            return arr[nextindex];
+        }
+        
+    }
+
+    int top(){
+         return arr[nextindex - 1];
+        
+    }
+
+    int size(){
+        return nextindex;
+    }
+
+    bool isempty(){
+        return nextindex == 0;   // if index  = 0 this will return true(1) otherwise false(0)
+    }
+};
+
+
+
+int main()
+{
+    stackusingarray<int> s1(5);
+  s1.push(10);
+  s1.push(20);
+  s1.push(30);
+  s1.push(40);
+  s1.push(50);
+  s1.push(60);
+
+  cout << "Top of stack is: " << s1.top() << endl;
+
+  cout << "The popped value is: " << s1.pop() << endl;
+  cout << "The popped value is: " << s1.pop() << endl;
+  cout << "The popped value is: " << s1.pop() << endl;
+
+  cout << "Top of stack is: " << s1.top() << endl;
+
+  cout << "The popped value is: " << s1.pop() << endl;
+  cout << "The popped value is: " << s1.pop() << endl;
+  cout << "The popped value is: " << s1.pop() << endl;
+
+  cout << "Top of stack is: " << s1.top() << endl;
+
+return 0;
+}
